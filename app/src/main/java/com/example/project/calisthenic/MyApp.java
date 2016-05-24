7269c137b807819cbe2cf9d61444995df073cd50
taitasciore <tatasciorecont@gmail.com>
2016-05-24 14:40:12 -0430

    Added custom fonts

---
+++ app/src/main/java/com/example/project/calisthenic/MyApp.java
@@ -0,0 +1,21 @@
+package com.example.project.calisthenic;
+
+import android.app.Application;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
+/**
+ * Created by saleventa7 on 5/24/2016.
+ */
+public class MyApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/OpenSans-Regular.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
+    }
+}
