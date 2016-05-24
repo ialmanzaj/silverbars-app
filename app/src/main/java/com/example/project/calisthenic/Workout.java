120e29dd2d70d608019c7149a3473591eb9882f9
Isaac Almanza <isaac.almanza19@gmail.com>
2016-05-24 11:55:20 -0500

    toolbar added

--- app/src/main/java/com/example/project/calisthenic/Workout.java
+++ app/src/main/java/com/example/project/calisthenic/Workout.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -51,7 +52,19 @@ public class Workout extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_workout);
+        // ======= TOOL BAR - BACK BUTTON  ADDED
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        if (myToolbar != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle("UPPER BODY");
+        }
+
+
 
         FloatingActionButton startButton = (FloatingActionButton) findViewById(R.id.fab);
         startButton.setOnClickListener(new View.OnClickListener() {
