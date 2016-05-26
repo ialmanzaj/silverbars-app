3a93b03ae6870f56bf415bb5e7e88cda6e1a7b90
andres934 <andresdrgarcia@hotmail.com>
2016-05-26 12:02:26 -0430

    onActiityResult

--- app/src/main/java/com/example/project/calisthenic/WorkoutActivity.java
+++ app/src/main/java/com/example/project/calisthenic/WorkoutActivity.java
@@ -332,11 +332,15 @@ public class WorkoutActivity extends AppCompatActivity {
         if (requestCode == 1 && resultCode == RESULT_OK && data != null){
             mySongs = (ArrayList<File>) data.getSerializableExtra("songs");
             position = data.getLongArrayExtra("positions");
+            toast("Success");
         }
         else if (requestCode == 1 && resultCode == RESULT_CANCELED) {
             mySongs = null;
             position = null;
+            toast("Canceled");
         }
+
+        toast("None of them");
     }
 
     @Override
