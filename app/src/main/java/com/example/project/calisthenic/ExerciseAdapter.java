23ef473e528ae06122d04706e11e55fd32c8db86
andres934 <andresdrgarcia@hotmail.com>
2016-05-27 12:12:30 -0430

    Soundcloud search working, change every exercise reps working

--- app/src/main/java/com/example/project/calisthenic/ExerciseAdapter.java
+++ app/src/main/java/com/example/project/calisthenic/ExerciseAdapter.java
@@ -1,13 +1,22 @@
 package com.example.project.calisthenic;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -48,10 +57,114 @@ public class ExerciseAdapter extends RecyclerView.Adapter<ExerciseAdapter.Exerci
     }
 
     @Override
-    public void onBindViewHolder(ExerciseViewHolder viewHolder, int i) {
-        viewHolder.imagen.setImageResource(items.get(i).getImagen());
-        viewHolder.nombre.setText(items.get(i).getNombre());
+    public void onBindViewHolder(final ExerciseViewHolder viewHolder, int i) {
+        final int a = i;
+        viewHolder.imagen.setImageResource(items.get(a).getImagen());
+        viewHolder.nombre.setText(items.get(a).getNombre());
 //        viewHolder.next.setText("Visitas:"+String.valueOf(items.get(i).getVisitas()));
-        viewHolder.repetitions.setText(items.get(i).getReps());
+        viewHolder.repetitions.setText(items.get(a).getReps());
+
+        viewHolder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            private TextView DialogName, Reps;
+            private Button plusRep, minusRep;
+            private int value = 0;
+
+            @Override
+            public boolean onLongClick(View view) {
+                boolean wrapInScrollView = true;
+                View v = new MaterialDialog.Builder(view.getContext())
+                        .title(R.string.title)
+                        .customView(R.layout.edit_reps_setup, wrapInScrollView)
+                        .positiveText("Done").onPositive(new MaterialDialog.SingleButtonCallback() {
+                            @Override
+                            public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                dialog.dismiss();
+                                viewHolder.repetitions.setText(String.valueOf(NewRepValue()));
+                            }
+                        })
+                        .show()
+                        .getCustomView();
+                DialogName = (TextView) v.findViewById(R.id.ExerciseName);
+                DialogName.setText(items.get(a).getNombre());
+                Reps = (TextView) v.findViewById(R.id.Reps);
+                Reps.setText(viewHolder.repetitions.getText());
+                plusRep = (Button) v.findViewById(R.id.plusRep);
+                plusRep.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        plusTempo(Reps,plusRep,minusRep);
+                    }
+                });
+                minusRep = (Button) v.findViewById(R.id.minusRep);
+                minusRep.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        minusTempo(Reps,minusRep,plusRep);
+                    }
+                });
+
+                return false;
+            }
+
+            public int NewRepValue(){
+                return Integer.valueOf(Reps.getText().toString());
+            }
+
+            public void plusTempo(TextView view, Button button, Button button2){
+                value = Integer.parseInt(view.getText().toString());
+                view.setText(String.valueOf(value+1));
+                value++;
+                if (view == Reps){
+                    if (value == 20){
+                        button.setEnabled(false);
+                        button.setClickable(false);
+                    }else{
+                        if(value > 1){
+                            button2.setEnabled(true);
+                            button2.setClickable(true);
+                        }
+                    }
+                }else{
+                    if (value == 10){
+                        button.setEnabled(false);
+                        button.setClickable(false);
+                    }else{
+                        if(value > 1){
+                            button2.setEnabled(true);
+                            button2.setClickable(true);
+                        }
+                    }
+                }
+
+            }
+
+            public void minusTempo(TextView view, Button button, Button button2){
+                value = Integer.parseInt(view.getText().toString());
+                view.setText(String.valueOf(value-1));
+                value--;
+                if (view == Reps){
+                    if ((value)==1){
+                        button.setEnabled(false);
+                        button.setClickable(false);
+                    }else{
+                        if(value < 20){
+                            button2.setEnabled(true);
+                            button2.setClickable(true);
+                        }
+                    }
+                }else{
+                    if ((value)==1){
+                        button.setEnabled(false);
+                        button.setClickable(false);
+                    }else{
+                        if(value < 10){
+                            button2.setEnabled(true);
+                            button2.setClickable(true);
+                        }
+                    }
+                }
+            }
+        });
     }
+
 }
