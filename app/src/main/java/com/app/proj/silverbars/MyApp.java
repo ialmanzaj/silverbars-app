dc7370e21216a3295049e9951e0dddfd696fcf5d
Isaac Almanza <isaac.almanza19@gmail.com>
2016-08-24 10:25:50 -0500

    bug en multi-indexing

--- app/src/main/java/com/app/proj/silverbars/MyApp.java
+++ app/src/main/java/com/app/proj/silverbars/MyApp.java
@@ -1,13 +1,14 @@
 package com.app.proj.silverbars;
 
 import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 /**
  * Created by roberto on 5/24/2016.
  */
-public class MyApp extends Application {
+public class MyApp extends MultiDexApplication {
 
     @Override
     public void onCreate() {
